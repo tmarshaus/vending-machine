8ccd468871d5106cb1fe798ec62c297dbab02495
Taylor <tmarshaus@gmail.com>
2020-10-08 09:53:50 -0400

    VM Framework set up

---
+++ 19_Capstone/Capstone/UI/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using MenuFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Capstone.UI
+{
+    public class MainMenu : ConsoleMenu
+    {
+    }
+}
---
+++ 19_Capstone/Capstone/UI/PurchaseMenu.cs
@@ -0,0 +1,11 @@
+﻿using MenuFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.UI
+{
+    public class PurchaseMenu : ConsoleMenu
+    {
+    }
+}
